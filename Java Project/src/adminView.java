cea7a51510e08ea20ffcce9944056029034f6596
DRUDILEA <drudilea@hotmail.com>
2018-05-31 13:29:45 -0300

    Actualizacion adminView

--- Java Project/src/adminView.java
+++ Java Project/src/adminView.java
@@ -1,13 +1,11 @@
-import java.awt.Dimension;
 import java.awt.EventQueue;
-import java.awt.Insets;
 import java.awt.SystemColor;
-import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.Font;
 import java.awt.GraphicsEnvironment;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -16,13 +14,9 @@ import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
-import javax.swing.table.JTableHeader;
-
 import javax.swing.JTabbedPane;
 import javax.swing.JTable;
 import javax.swing.JScrollPane;
-import javax.swing.ScrollPaneConstants;
-import javax.swing.JComboBox;
 import javax.swing.ListSelectionModel;
 
 public class adminView extends JFrame {
@@ -50,47 +44,46 @@ public class adminView extends JFrame {
 	 */
 	public adminView() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		//setBounds(600, 300, 650, 400);
-		
-		/*Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		Insets scnMax = Toolkit.getDefaultToolkit().getScreenInsets(getGraphicsConfiguration());
-		int taskBarSize = scnMax.bottom;
-		setSize(screenSize.width - getWidth(), screenSize.height - taskBarSize - getHeight());*/
 		
 		GraphicsEnvironment env = GraphicsEnvironment.getLocalGraphicsEnvironment();
 		this.setMaximizedBounds(env.getMaximumWindowBounds());
         this.setExtendedState(this.getExtendedState() | this.MAXIMIZED_BOTH);
 		
+        //Contenedor/Panel General
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
+		//Panel titulo
 		JPanel panel = new JPanel();
 		panel.setBounds(82, 0, 1288, 77);
 		panel.setBackground(SystemColor.inactiveCaption);
 		contentPane.add(panel);
 		panel.setLayout(null);
 		
+		//Etiqueta de titulo
 		JLabel lblNewLabel = new JLabel("Administración");
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setBounds(10, 0, 502, 71);
 		panel.add(lblNewLabel);
 		lblNewLabel.setFont(new Font("Yu Gothic UI Semilight", Font.BOLD, 48));
 		
+		//Panel boton "Atras"
 		JPanel panel_1 = new JPanel();
 		panel_1.setBounds(0, 0, 84, 77);
 		panel_1.setBackground(SystemColor.activeCaption);
 		contentPane.add(panel_1);
 		panel_1.setLayout(null);
 		
+		//Boton "Atras"
 		JButton backButton = new JButton("ATRAS");
 		backButton.setFont(new Font("Yu Gothic UI Semilight", Font.BOLD, 11));
 		backButton.setForeground(SystemColor.activeCaptionText);
 		backButton.setBackground(SystemColor.activeCaption);
 	    backButton.setVerticalTextPosition(SwingConstants.BOTTOM);
 	    backButton.setHorizontalTextPosition(SwingConstants.CENTER);
-		//backButton.setIcon(new ImageIcon(AgendaDeTurnos.class.getResource("/images/espalda.png")));
+		backButton.setIcon(new ImageIcon(adminView.class.getResource("/images/espalda.png")));
 		backButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
@@ -98,73 +91,165 @@ public class adminView extends JFrame {
 		backButton.setBounds(0, 0, 84, 77);
 		panel_1.add(backButton);
 		
+		//Etiqueta "Atras"
 		JLabel lblAtras = new JLabel("ATRAS");
 		lblAtras.setBounds(0, 63, 62, 14);
 		panel_1.add(lblAtras);
 		lblAtras.setForeground(SystemColor.desktop);
 		lblAtras.setHorizontalAlignment(SwingConstants.CENTER);
 		
+		
+		//Panel de pestañas
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		tabbedPane.setBackground(SystemColor.inactiveCaption);
 		tabbedPane.setFont(new Font("Yu Gothic UI Semilight", Font.PLAIN, 14));
 		tabbedPane.setBounds(240, 113, 1120, 576);
 		contentPane.add(tabbedPane);
 		
+		//Pestaña "Asistencia"
 		JPanel asist = new JPanel();
 		tabbedPane.addTab("Asistencia", null, asist, null);
 		
+		//Pestaña "Posicion Consolidada"
 		JPanel posCons = new JPanel();
 		tabbedPane.addTab("Posicion Consolidada", null, posCons, null);
 		posCons.setLayout(null);
 		
+		//Panel general de la pestaña "Posicion Consolidada"
 		JPanel panel_3 = new JPanel();
 		panel_3.setBounds(10, 40, 1095, 95);
 		posCons.add(panel_3);
 		panel_3.setLayout(null);
 		
+		//Etiqueta "$"
 		JLabel label = new JLabel("$");
 		label.setHorizontalAlignment(SwingConstants.CENTER);
 		label.setFont(new Font("Yu Gothic UI Semilight", Font.BOLD, 75));
 		label.setBounds(0, 0, 100, 95);
 		panel_3.add(label);
 		
+		//Texto de Saldo final
 		JLabel lblNewLabel_2 = new JLabel("16.000");
 		lblNewLabel_2.setFont(new Font("Yu Gothic UI Semilight", Font.BOLD, 75));
 		lblNewLabel_2.setHorizontalAlignment(SwingConstants.LEFT);
-		lblNewLabel_2.setBounds(100, 0, 1015, 95);
+		lblNewLabel_2.setBounds(100, 0, 795, 95);
 		panel_3.add(lblNewLabel_2);
 		
+		//Panel para etiqueta "Saldo"
 		JPanel panel_4 = new JPanel();
 		panel_4.setBounds(10, 0, 270, 40);
 		posCons.add(panel_4);
 		panel_4.setLayout(null);
 		
+		//Etiqueta "Saldo"
 		JLabel lblNewLabel_3 = new JLabel("Saldo");
 		lblNewLabel_3.setFont(new Font("Yu Gothic UI Semilight", Font.PLAIN, 35));
 		lblNewLabel_3.setBounds(25, 0, 245, 40);
 		panel_4.add(lblNewLabel_3);
 		
+		//Panel para Headers de la tabla
 		JPanel panel_5 = new JPanel();
-		panel_5.setBounds(10, 145, 1095, 40);
+		panel_5.setBounds(10, 145, 895, 40);
 		posCons.add(panel_5);
 		panel_5.setLayout(null);
 		
+		//Header "Tratamiento"
 		JLabel lblNewLabel_4 = new JLabel("Tratamiento");
 		lblNewLabel_4.setFont(new Font("Yu Gothic UI Semilight", Font.PLAIN, 20));
-		lblNewLabel_4.setBounds(0, 0, 925, 40);
+		lblNewLabel_4.setBounds(0, 0, 725, 40);
 		panel_5.add(lblNewLabel_4);
 		
-		JLabel lblNewLabel_5 = new JLabel("Monto");
+		//Header "Importe"
+		JLabel lblNewLabel_5 = new JLabel("Importe");
 		lblNewLabel_5.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel_5.setFont(new Font("Yu Gothic UI Semilight", Font.PLAIN, 20));
-		lblNewLabel_5.setBounds(925, 0, 150, 40);
+		lblNewLabel_5.setBounds(725, 0, 150, 40);
 		panel_5.add(lblNewLabel_5);
 		
+		//Tabla para tratamiento e importe
+		JTable table2 = new JTable();
+		table2.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		table2.setRowSelectionAllowed(false);
+		table2.setFont(new Font("Yu Gothic UI Semilight", Font.PLAIN, 16));
+		table2.setBackground(SystemColor.activeCaption);
+		table2.setModel(new DefaultTableModel(
+			new Object[][] {
+				{new String ("Extracción Premolar"), new Integer(-2500)},
+				{new String ("Limpieza bucal"), new Integer(-500)},
+				{new String ("Depósito"), new Integer(10000)}, 
+				{null, null},
+				{null, null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				{null},
+				
+			},
+			new String[] {"", ""}
+		) {
+			/**
+			 * 
+			 */
+			private static final long serialVersionUID = 1L;
+			
+			
+			boolean[] columnEditables = new boolean[] {
+				false
+				};
+			
+			public boolean isCellEditable(int row, int column) {
+				return columnEditables[column];
+			}
+		});
+		table2.getColumnModel().getColumn(0).setPreferredWidth(650);
+				
+		DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
+		centerRenderer.setHorizontalAlignment(JLabel.CENTER);
+		table2.getColumnModel().getColumn(1).setCellRenderer(centerRenderer);
+				
+		//Panel con scroll para la tabla de tratamientos e importe
+		JScrollPane scrollPane_1 = new JScrollPane(table2);
+		scrollPane_1.setBounds(10, 185, 895, 345);
+		posCons.add(scrollPane_1);
 		
+		
+		
+		
+		//Pestaña "Impresion"
 		JPanel impr = new JPanel();
 		tabbedPane.addTab("Impresion", null, impr, null);
 		
-	
+
+		//Tabla de Pacientes
 		JTable table = new JTable();
 		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		table.setRowSelectionAllowed(false);
@@ -233,97 +318,18 @@ public class adminView extends JFrame {
 		rendere.setToolTipText("Doble click para seleccionar paciente");
 		table.getColumnModel().getColumn(0).setCellRenderer(rendere);
 
-		
+		//Panel con scroll para Pacientes
 		JScrollPane scrollPane = new JScrollPane(table);
 		scrollPane.setBounds(10, 144, 227, 545);
 		contentPane.add(scrollPane);
 		
-		
-		
-		JTable table2 = new JTable();
-		table2.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		table2.setRowSelectionAllowed(false);
-		table2.setFont(new Font("Yu Gothic UI Semilight", Font.PLAIN, 16));
-		table2.setBackground(SystemColor.activeCaption);
-		table2.setModel(new DefaultTableModel(
-			new Object[][] {
-				{new String ("Extracción Premolar"), new Integer(-2500)},
-				{new String ("Limpieza bucal"), new Integer(-500)},
-				{new String ("Depósito"), new Integer(10000)}, 
-				{null, null},
-				{null, null},
-				{null},
-				{null},
-				{null},
-				{null},
-				{null},
-				{null},
-				{null},
-				{null},
-				{null},
-				{null},
-				{null},
-				{null},
-				{null},
-				{null},
-				{null},
-				{null},
-				{null},
-				{null},
-				{null},
-				{null},
-				{null},
-				{null},
-				{null},
-				{null},
-				{null},
-				{null},
-				{null},
-				{null},
-				{null},
-				{null},
-				{null},
-				{null},
-				{null},
-				
-			},
-			new String[] {"", ""}
-		) {
-			/**
-			 * 
-			 */
-			private static final long serialVersionUID = 1L;
-			
-			
-			boolean[] columnEditables = new boolean[] {
-				false
-				};
-			
-			public boolean isCellEditable(int row, int column) {
-				return columnEditables[column];
-			}
-		});
-		table2.getColumnModel().getColumn(0).setPreferredWidth(850);
-				
-		DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
-		//DefaultTableCellRenderer leftRenderer = new DefaultTableCellRenderer();
-		centerRenderer.setHorizontalAlignment(JLabel.CENTER);
-		//leftRenderer.setHorizontalAlignment(JLabel.LEFT);
-		//table2.getColumnModel().getColumn(0).setHeaderRenderer(leftRenderer);
-		//table2.getColumnModel().getColumn(1).setHeaderRenderer(leftRenderer);
-		table2.getColumnModel().getColumn(1).setCellRenderer(centerRenderer);
-				
-		
-		JScrollPane scrollPane_1 = new JScrollPane(table2);
-		scrollPane_1.setBounds(10, 185, 1095, 345);
-		posCons.add(scrollPane_1);
-		
-		
+		//Panel para etiqueta Pacientes		
 		JPanel panel_2 = new JPanel();
 		panel_2.setBounds(10, 101, 227, 37);
 		contentPane.add(panel_2);
 		panel_2.setLayout(null);
 		
+		//Etiqueta "Pacientes"
 		JLabel lblNewLabel_1 = new JLabel("Pacientes");
 		lblNewLabel_1.setFont(new Font("Yu Gothic UI Semilight", Font.PLAIN, 27));
 		lblNewLabel_1.setBounds(0, 0, 227, 37);
